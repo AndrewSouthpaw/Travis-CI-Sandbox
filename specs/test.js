d22946bf34f8a402d5f70347fd282b1d6f6361de
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-01-20 17:11:19 -0800

    setting up testing environment

---
+++ specs/test.js
@@ -0,0 +1,9 @@
+
+describe('a test', function () {
+  it('should be true', function () {
+    expect(true).to.equal(true);
+  });
+  it('should run again on creating new test', function () {
+    expect(true).to.equal(true);
+  });
+});
